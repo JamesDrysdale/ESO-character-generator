e52363a0a8f4d6bf0ede5b9f34dd37c5fba3840b
JamesDrysdale <jamesdrysdale84@gmail.com.com>
2020-12-08 12:18:37 +0000

    Fixed indentation and whitespace for better readability

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,30 +1,32 @@
 <template>
-<div id="app">
-  <CharacterViewer/>
-  <CharacterCreator/>
-</div>
+  <div id="app">
+    <CharacterViewer/>
+    <CharacterCreator/>
+  </div>
 </template>
 
 <script>
-import CharacterViewer from './components/CharacterViewer.vue'
-import CharacterCreator from './components/CharacterCreator.vue'
+  import CharacterViewer from './components/CharacterViewer.vue'
+  import CharacterCreator from './components/CharacterCreator.vue'
 
-export default {
-  name: 'App',
-  components: {
-    CharacterViewer,
-    CharacterCreator
+  export default {
+    name: 'App',
+    components: {
+      CharacterViewer,
+      CharacterCreator
+    }
   }
-}
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
+  
 </style>
