ffb0436cad2adc1656cd570e7e002d4745412244
JamesDrysdale <jamesdrysdale84@gmail.com.com>
2020-12-08 13:30:52 +0000

    App has two pages with links that navigate between them, and basic styling on the navigation bar

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,7 +1,20 @@
 <template>
   <div id="app">
-    <CharacterViewer/>
-    <CharacterCreator/>
+
+    <header>
+      <nav>
+        <ul>
+          <li><a v-on:click="toggle='character-viewer'">View all characters</a></li>
+          <li><a v-on:click="toggle='character-creator'">Create a character</a></li>
+        </ul>
+      </nav>
+    </header>
+
+    <main>
+      <CharacterViewer v-show="toggle==='character-viewer'"/>
+      <CharacterCreator v-show="toggle==='character-creator'"/>
+    </main>
+
   </div>
 </template>
 
@@ -14,6 +27,11 @@
     components: {
       CharacterViewer,
       CharacterCreator
+    },
+    data: function() {
+      return {
+        toggle: "character-viewer"
+      }
     }
   }
 </script>
@@ -28,5 +46,30 @@
     color: #2c3e50;
     margin-top: 60px;
   }
-  
+
+  ul {
+    list-style-type: none;
+    margin: 0;
+    padding: 0;
+    overflow: hidden;
+    background-color: #333;
+  } 
+
+  li {
+    float: right;
+  }
+
+  li a {
+    display: block;
+    color: white;
+    text-align: center;
+    padding: 14px 16px;
+    text-decoration: none;
+  }
+
+  /* Change the link color to #111 (black) on hover */
+  li a:hover {
+    background-color: #111;
+  } 
+
 </style>
